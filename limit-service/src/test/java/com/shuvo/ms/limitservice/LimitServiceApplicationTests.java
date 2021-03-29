7667f7c21ab5221b5d8054fa4023c36839ac934d
Shuvo Jyoti Banerjee <shuvojyotiprl@gmail.com>
2021-03-29 20:16:47 +0530

    fixed the config server and client issue

---
+++ limit-service/src/test/java/com/shuvo/ms/limitservice/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuvo.ms.limitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-cloud-config-server/src/test/java/com/shuvo/ms/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuvo.ms.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
